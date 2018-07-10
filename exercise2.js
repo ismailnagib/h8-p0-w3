eb6d2e296780ad22061659980dc2c7f5e2ed390f
ismail <mismailyusuf@gmail.com>
2018-07-11 02:06:02 +0700

    add exercise2

---
+++ exercise2.js
@@ -0,0 +1,8 @@
+function balikString(input) {
+    var output = "";
+    for (var i = input.length - 1; i >= 0; i--) {
+        output += input[i];
+    }
+    return(output);
+}
+console.log(balikString("hello world!"));
